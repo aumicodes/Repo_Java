9957d56c59e51820fa42e5cf60d95b601f619910
aumialam <aumialam@gmail.com>
2022-03-07 19:48:18 -0500

    First homework2

---
+++ src/Homework2.java
@@ -0,0 +1,38 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+
+        byte numberOfHoursInADay = 24;
+        System.out.println("Number of hours in a day is = " +numberOfHoursInADay);
+
+        short maxNumberOfDaysInAYear = 366;
+        System.out.println("Max number of days in a year is = " +maxNumberOfDaysInAYear);
+
+        short numbersOfEmployeesInAnOrganization = 5000;
+        System.out.println("Number of employees in an organization is = " +numbersOfEmployeesInAnOrganization);
+
+        int populationInACountry = 2000000;
+        System.out.println("Population of a country is = " +populationInACountry);
+
+        short mortgageRateIs = 3500;
+        System.out.println("Mortgage rate is = $" +mortgageRateIs+ " per month");
+
+        double balanceInBankAccount = 1000000.50;
+        System.out.println("Balance in bank account is = $" + balanceInBankAccount);
+
+        boolean sunRiseFromWest = false;
+        System.out.println("Does the sun rise from the west? = " + sunRiseFromWest);
+
+        char firstInitial = 'A';
+        char secondInitial = 'A';
+        System.out.println("My initials are " +firstInitial + secondInitial);
+
+        String myName = "Aumi Alam";
+        System.out.println("My name is " +myName);
+
+    }
+
+
+    }
